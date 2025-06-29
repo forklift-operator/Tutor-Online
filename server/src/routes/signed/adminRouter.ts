d3215a9be02a9d4ae0a9f00ea62c95e2403c2f3e
Stoyan Yordanov <137334856+forklift-operator@users.noreply.github.com>
2025-06-30 00:15:26 +0300

    Fixed admin router to return

--- server/src/routes/signed/adminRouter.ts
+++ server/src/routes/signed/adminRouter.ts
@@ -5,25 +5,34 @@ import { Request, Response, NextFunction } from 'express';
 
 const adminRouter = express.Router();
 
-adminRouter.get('/users', validateToken, checkRole(['admin']), async (req: Request, res: Response, next: NextFunction) => {
+adminRouter.get('/users', validateToken, checkRole(['admin']), async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
         const query = req.query.name as string;
         const filter = query ? { username: { $regex: query, $options: 'i' } } : {};
         const users = await UserModel.find(filter);
-        res.status(200).json(users);
+        return res.status(200).json(users);
     } catch (e) {
-        next(e);
+        return next(e);
     }
-}).delete('/users/:userId', validateToken, checkRole(['admin']), async (req: Request, res: Response, next: NextFunction) => {
+}).put('/users', validateToken, checkRole(['admin']), async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    try {
+        const updated = await UserModel.findByIdAndUpdate(req.body._id, req.body, { new: true });
+        return res.status(200).json(updated);
+    } catch (e) {
+        console.error((e as Error).message);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+
+}).delete('/users/:userId', validateToken, checkRole(['admin']), async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
         const userId = req.params.userId;
         const deleted = await UserModel.deleteOne({ _id: userId });
         if (deleted.deletedCount === 0) {
-            res.status(400).json({ message: "User doesn't exist" })
+            return res.status(400).json({ message: "User doesn't exist" })
         }
-        res.status(200).json(deleted);
+        return res.status(200).json(deleted);
     } catch (e) {
-        next(e);
+        return next(e);
     }
 })
 
